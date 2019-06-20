1c52ed097c75e5f481be5ee22bd60d13f2cd2d03
Vixtor61 <00198117@uca.edu.sv>
2019-06-20 17:00:46 -0600

    DATABASE DONE

--- DATABASE.sql
+++ DATABASE.sql
@@ -1,3 +1,6 @@
+CREATE DATABASE proyecto
+use proyecto
+
 CREATE TABLE municipio(
 	id INT NOT NULL,
 	nombre VARCHAR(20)
@@ -10,6 +13,7 @@ CREATE TABLE hospital(
 	id INT NOT NULL,
 	nombre VARCHAR(20),
 	direccion VARCHAR(50),
+	telefono VARCHAR(8),
 	id_municipio INT,
 	CONSTRAINT pk_hospital PRIMARY KEY (id),
 	CONSTRAINT fk_municipio FOREIGN KEY(id_municipio) REFERENCES municipio(id)
@@ -25,11 +29,11 @@ CREATE TABLE equipo_medico(
 	CONSTRAINT pk_equipoMedico PRIMARY KEY (numero_serie)
 	);
 
-CREATE TABLE accesorios(
+CREATE TABLE accesorios_em(
 	id INT NOT NULL,
 	nombre VARCHAR(20),
 	id_equipoMedico INT,
-	CONSTRAINT pk_accesorios PRIMARY KEY (id),
+	CONSTRAINT pk_accesorios_em PRIMARY KEY (id),
 	CONSTRAINT fk_equipo_medico FOREIGN KEY(id_equipoMedico) REFERENCES equipo_medico(numero_serie)
 	);
 
@@ -57,11 +61,74 @@ CREATE TABLE inspeccion(
 	);
 
 CREATE TABLE EMXINS(
+	id_em INT NOT NULL,
+	id_ins INT NOT NULL,
+	fecha_hora_inspeccion DATETIME,
+	
+	CONSTRAINT fk_em_emxins FOREIGN KEY(id_em) REFERENCES equipo_medico(numero_serie),
+	CONSTRAINT fk_ins_emxins FOREIGN KEY(id_ins) REFERENCES inspeccion(id),
+	unique(id_em,id_ins)
+	);
+
+CREATE TABLE puntos_medicion(
 	id INT NOT NULL,
 	nombre VARCHAR(20),
-	id_equipoMedico INT,
-	CONSTRAINT pk_tipoEM PRIMARY KEY (id),
-	CONSTRAINT fk_equipo__medico FOREIGN KEY(id_equipoMedico) REFERENCES equipo_medico(numero_serie)
+	id_inspeccion INT,
+	CONSTRAINT PK_puntos_medicion PRIMARY KEY(id),
+	CONSTRAINT FK_inspeccion_pmedicion FOREIGN KEY(id_inspeccion) REFERENCES inspeccion(id)
+
+);
+
+CREATE TABLE parametro(
+	id INT NOT NULL,
+	nombre VARCHAR(20),
+	id_inspeccion INT,
+	CONSTRAINT PK_parametro PRIMARY KEY(id),
+	CONSTRAINT FK_inspeccion_parametro FOREIGN KEY(id_inspeccion) REFERENCES inspeccion(id)
+
+);
+
+CREATE TABLE equipo_inspeccion(
+	no_serie INT NOT NULL,
+	nombre VARCHAR(20),
+	marca VARCHAR(20),
+	modelo VARCHAR(20),
+	fecha_calibracion DATE,
+	certificado_calibracion VARCHAR(20),
+	CONSTRAINT PK_equipo_inspeccion PRIMARY KEY(no_serie)
+);
+
+
+CREATE TABLE INSXEI(
+	id_ins INT not null,
+	id_ei INT not null,
+
+	CONSTRAINT FK_ins_insxei  FOREIGN KEY (id_ins) REFERENCES inspeccion(id),
+	
+	CONSTRAINT FK_ei_insxei  FOREIGN KEY (id_ei) REFERENCES equipo_inspeccion(no_serie),
+	UNIQUE(id_ins,id_ei)
+);
+
+CREATE TABLE accesorios_ins(
+	id INT NOT NULL,
+	nombre VARCHAR(20),
+	id_equipo_inspeccion INT,
+	CONSTRAINT pk_accesorios_ins PRIMARY KEY (id),
+	CONSTRAINT fk_equipo_inspeccion FOREIGN KEY(id_equipo_inspeccion) REFERENCES equipo_inspeccion(no_serie)
+);
+
+CREATE TABLE area(
+	id INT NOT NULL,
+	nombre VARCHAR(20),
+	servicio_hospitalario VARCHAR(50),
+	responsable VARCHAR(50),
+	responsable_mantenimiento VARCHAR(50),
+	extension VARCHAR(4),
+	id_hospital INT,
+	id_em INT
+	CONSTRAINT pk_area PRIMARY KEY (id),
+	CONSTRAINT fk_equipo_medico_area FOREIGN KEY(id_em) REFERENCES equipo_medico(numero_serie),
+	CONSTRAINT fk_hospital_area FOREIGN KEY(id_hospital) REFERENCES hospital(id)
 	);
 
 
